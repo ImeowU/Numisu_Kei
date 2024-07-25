4096dd86c4dd535645b164ad5e27f669ffa14121
Numisu Kei <127876547+ImeowU@users.noreply.github.com>
2024-07-25 19:56:11 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,184 @@
+'use strict';
+
+// element toggle function
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+
+// sidebar variables
+const sidebar = document.querySelector("[data-sidebar]");
+const sidebarBtn = document.querySelector("[data-sidebar-btn]");
+
+// sidebar toggle functionality for mobile
+if (sidebar && sidebarBtn) {
+  sidebarBtn.addEventListener("click", function () { elementToggleFunc(sidebar); });
+}
+
+// testimonials variables
+const testimonialsItem = document.querySelectorAll("[data-testimonials-item]");
+const modalContainer = document.querySelector("[data-modal-container]");
+const modalCloseBtn = document.querySelector("[data-modal-close-btn]");
+const overlay = document.querySelector("[data-overlay]");
+
+// modal variable
+const modalImg = document.querySelector("[data-modal-img]");
+const modalTitle = document.querySelector("[data-modal-title]");
+const modalText = document.querySelector("[data-modal-text]");
+
+// modal toggle function
+const testimonialsModalFunc = function () {
+  if (modalContainer && overlay) {
+    modalContainer.classList.toggle("active");
+    overlay.classList.toggle("active");
+  }
+}
+
+// add click event to all modal items
+if (testimonialsItem && modalImg && modalTitle && modalText) {
+  for (let i = 0; i < testimonialsItem.length; i++) {
+    testimonialsItem[i].addEventListener("click", function () {
+      const avatar = this.querySelector("[data-testimonials-avatar]");
+      const title = this.querySelector("[data-testimonials-title]");
+      const text = this.querySelector("[data-testimonials-text]");
+
+      if (avatar && title && text) {
+        modalImg.src = avatar.src;
+        modalImg.alt = avatar.alt;
+        modalTitle.innerHTML = title.innerHTML;
+        modalText.innerHTML = text.innerHTML;
+        testimonialsModalFunc();
+      } else {
+        console.error("Element not found");
+      }
+    });
+  }
+}
+
+// add click event to modal close button
+if (modalCloseBtn && overlay) {
+  modalCloseBtn.addEventListener("click", testimonialsModalFunc);
+  overlay.addEventListener("click", testimonialsModalFunc);
+}
+
+// custom select variables
+const select = document.querySelector("[data-select]");
+const selectItems = document.querySelectorAll("[data-select-item]");
+const selectValue = document.querySelector("[data-select-value]"); 
+const filterBtn = document.querySelectorAll("[data-filter-btn]");
+
+if (select && selectValue) {
+  select.addEventListener("click", function () { elementToggleFunc(this); });
+
+  // add event in all select items
+  for (let i = 0; i < selectItems.length; i++) {
+    selectItems[i].addEventListener("click", function () {
+      let selectedValue = this.innerText.toLowerCase();
+      selectValue.innerText = this.innerText;
+      elementToggleFunc(select);
+      filterFunc(selectedValue);
+    });
+  }
+}
+
+// filter variables
+const filterItems = document.querySelectorAll("[data-filter-item]");
+
+const filterFunc = function (selectedValue) {
+  for (let i = 0; i < filterItems.length; i++) {
+    if (selectedValue === "all") {
+      filterItems[i].classList.add("active");
+    } else if (selectedValue === filterItems[i].dataset.category) {
+      filterItems[i].classList.add("active");
+    } else {
+      filterItems[i].classList.remove("active");
+    }
+  }
+}
+
+// add event in all filter button items for large screen
+if (filterBtn && filterBtn.length > 0 && selectValue) {
+  let lastClickedBtn = filterBtn[0];
+  
+  for (let i = 0; i < filterBtn.length; i++) {
+    filterBtn[i].addEventListener("click", function () {
+      let selectedValue = this.innerText.toLowerCase();
+      selectValue.innerText = this.innerText;
+      filterFunc(selectedValue);
+  
+      lastClickedBtn.classList.remove("active");
+      this.classList.add("active");
+      lastClickedBtn = this;
+    });
+  }
+}
+
+// contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add event to all form input field
+if (form && formBtn && formInputs) {
+  for (let i = 0; i < formInputs.length; i++) {
+    formInputs[i].addEventListener("input", function () {
+      if (form.checkValidity()) {
+        formBtn.removeAttribute("disabled");
+      } else {
+        formBtn.setAttribute("disabled", "");
+      }
+    });
+  }
+}
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// add event to all nav link
+if (navigationLinks.length > 0 && pages.length > 0) {
+  for (let i = 0; i < navigationLinks.length; i++) {
+    navigationLinks[i].addEventListener("click", function () {
+      for (let j = 0; j < pages.length; j++) {
+        if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
+          pages[j].classList.add("active");
+          navigationLinks[j].classList.add("active");
+          window.scrollTo(0, 0);
+        } else {
+          pages[j].classList.remove("active");
+          navigationLinks[j].classList.remove("active");
+        }
+      }
+    });
+  }
+}
+
+// send email
+const form_contact = document.querySelector("form");
+const fullName = document.getElementById("fullname_id");
+const email = document.getElementById("email_id");
+const message = document.getElementById("message_id");
+
+function sendEmail(){
+  const bodyMessage = `Full Name:${fullName.value} <br> Email: ${email.value} <br> Message: ${message.value}`
+  Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "ryujikeita0203@gmail.com",
+    Password : "3887CE1405F8E28D069517F952FF787F50EE",
+    To : 'ryujikeita0203@gmail.com',
+    From : "ryujikeita0203@gmail.com",
+    Subject : "This is the subject",
+    Body : bodyMessage
+}).then(
+  message => {
+    if(message == "OK"){
+      Swal.fire({
+        title: "Send Successfully!",
+        text: "Thank you visited my website",
+        icon: "success"
+      });
+    }
+  }
+);
+}
+form_contact.addEventListener("submit", (e)=>{
+  e.preventDefault();
+  sendEmail();
+})
